8134ef47a43eb5b8786adbaef4f395e0d0dfd81c
Khang Nguyen <khangie@hotmail.com>
2020-08-10 15:44:01 -0700

    added type inference

---
+++ src/io/javabrains/TypeInferenceExample.java
@@ -0,0 +1,20 @@
+package io.javabrains;
+
+public class TypeInferenceExample {
+
+    public static void main(String[] args) {
+
+        StringLengthLambda myLambda = s -> s.length();
+        System.out.println(myLambda.getLength("Hello Lambda")); // Lambda function behaves like an implementation of the interface
+        printLambda(myLambda);
+    }
+
+    public static void printLambda (StringLengthLambda l) {
+        System.out.println(l.getLength("Hello Lambda"));
+    }
+
+    interface StringLengthLambda {
+        int getLength(String s);
+    }
+
+}
